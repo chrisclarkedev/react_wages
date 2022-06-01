5236c15ead6a597758e2823001fbc7fbfa4e12cf
chrisclarkedev <cclarke8904@gmail.com>
2022-06-01 12:36:18 -0400

    added income and parse through every income to add price

--- src/App.js
+++ src/App.js
@@ -10,11 +10,23 @@ function App() {
   const [totalIncome, setTotalIncome] = useState(0);
   //Array of Incomes Total Incomes
 
+  useEffect(() => {
+    let temp = 0;
+    for (let i = 0; i < income.length; i++) {
+      temp += parseInt(income[i].price);
+      // Parse through every single income and add price up
+    }
+
+    setTotalIncome(temp);
+  }, [income]);
+
+  //Only process useEffect when it first starts and when income changes
+
   return (
     <div className="App">
       <Header totalIncome={totalIncome} />
       <WageForm income={income} setIncome={setIncome} />
-      {income.length > 0 ? income[0].desc : ''}
+      {/* {income.length > 0 ? income[0].desc : ''} */}
     </div>
   );
 }
